41497675640cde4b054fc9d97f9b930df4ab09ad
zilongwang1993 <saberdoodle@gmail.com>
2014-12-01 17:25:12 -0600

    working on login

--- VirtualBB/ViewController.h
+++ VirtualBB/ViewController.h
@@ -14,6 +14,7 @@
 
 @property (strong, nonatomic) IBOutlet UITextField *txtPassword;
 
+- (IBAction)signIn:(id)sender;
 
 @end
 
--- VirtualBB/ViewController.m
+++ VirtualBB/ViewController.m
@@ -12,11 +12,95 @@
 
 @end
 
+
 @implementation ViewController
+@synthesize txtUsername, txtPassword;
 
 - (IBAction)signIn:(id)sender {
-    
+    NSLog(@"The user name is: %@ and the password is %@",txtUsername.text,txtPassword.text);
+    NSInteger success = 0;
+    @try {
+        
+        if([[self.txtUsername text] isEqualToString:@""] || [[self.txtPassword text] isEqualToString:@""] ) {
+            
+            [self popMsg:@"Please enter Email and Password" :@"Sign in Failed!" :0];
+            
+        } else {
+            NSString *post =[[NSString alloc] initWithFormat:@"username=%@&password=%@",[self.txtUsername text],[self.txtPassword text]];
+            NSLog(@"PostData: %@",post);
+            
+            NSURL *url=[NSURL URLWithString:@"http://dipinkrishna.com/jsonlogin.php"];
+            
+            NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+            
+            NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
+            
+            NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+            [request setURL:url];
+            [request setHTTPMethod:@"POST"];
+            [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+            [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+            [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+            [request setHTTPBody:postData];
+            
+            //[NSURLRequest setAllowsAnyHTTPSCertificate:YES forHost:[url host]];
+            
+            NSError *error = [[NSError alloc] init];
+            NSHTTPURLResponse *response = nil;
+            NSData *urlData=[NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+            
+            NSLog(@"Response code: %ld", (long)[response statusCode]);
+            
+            if ([response statusCode] >= 200 && [response statusCode] < 300)
+            {
+                NSString *responseData = [[NSString alloc]initWithData:urlData encoding:NSUTF8StringEncoding];
+                NSLog(@"Response ==> %@", responseData);
+                
+                NSError *error = nil;
+                NSDictionary *jsonData = [NSJSONSerialization
+                                          JSONObjectWithData:urlData
+                                          options:NSJSONReadingMutableContainers
+                                          error:&error];
+                
+                success = [jsonData[@"success"] integerValue];
+                NSLog(@"Success: %ld",(long)success);
+                
+                if(success == 1)
+                {
+                    NSLog(@"Login SUCCESS");
+                } else {
+                    
+                    NSString *error_msg = (NSString *) jsonData[@"error_message"];
+                   [self popMsg:error_msg :@"Sign in Failed!" :0];
+                }
+                
+            } else {
+                //if (error) NSLog(@"Error: %@", error);
+               [self popMsg:@"Connection Failed" :@"Sign in Failed!" :0];
+            }
+        }
+    }
+    @catch (NSException * e) {
+        NSLog(@"Exception: %@", e);
+        [self popMsg:@"Sign in Failed." :@"Error!" :0];
+    }
+    if (success) {
+        [self performSegueWithIdentifier:@"login_success" sender:self];
+    }
 }
+
+
+- (void) popMsg:(NSString *)msg :(NSString *)title :(int) tag
+{
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:title
+                                                        message:msg
+                                                       delegate:self
+                                              cancelButtonTitle:@"Ok"
+                                              otherButtonTitles:nil, nil];
+    alertView.tag = tag;
+    [alertView show];
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
