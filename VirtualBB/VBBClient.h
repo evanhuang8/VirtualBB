04149946fc0c0e284c8c2a70023113be5775533e
Evan Huang <evan.huang@idcprojects.com>
2014-12-01 16:35:16 -0600

    VBBClient

---
+++ VirtualBB/VBBClient.h
@@ -0,0 +1,13 @@
+//
+//  VBBClient.h
+//  VirtualBB
+//
+//  Created by Evan Huang on 12/1/14.
+//  Copyright (c) 2014 Washington University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VBBClient : NSObject
+
+@end
---
+++ VirtualBB/VBBClient.m
@@ -0,0 +1,13 @@
+//
+//  VBBClient.m
+//  VirtualBB
+//
+//  Created by Evan Huang on 12/1/14.
+//  Copyright (c) 2014 Washington University. All rights reserved.
+//
+
+#import "VBBClient.h"
+
+@implementation VBBClient
+
+@end
